34f3bf46fc7622b630a9b55410ded5ef2a5aed3a
Emerson Braga <76827685+emersubraga@users.noreply.github.com>
2025-02-09 18:48:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
 async function gerarDocumento(modeloArrayBuffer, empresa, nota, ordem, data, imagens) {
     const { Document, Packer, Paragraph, TextRun, ImageRun } = docx;
 
-    // const doc = new Document({
-    //     sections: [{
-    //         properties: {},
-    //         children: [
-    //             new Paragraph({
-    //                 children: [
-    //                     new TextRun({ text: `Empresa: ${empresa}`, bold: true }),
-    //                     new TextRun(`\nNota Fiscal: ${nota}`),
-    //                     new TextRun(`\nOrdem de C/S: ${ordem}`),
-    //                     new TextRun(`\nFaturamento Mês/Ano: ${data}`)
-    //                 ]
-    //             }),
-    //             ...imagens.map(imgSrc => new Paragraph({
-    //                 children: [new ImageRun({
-    //                     data: imgSrc,
-    //                     transformation: { width: 400, height: 200 }
-    //                 })]
-    //             }))
-    //         ]
-    //     }]
-    // });
-
     const doc = new Document({
-    sections: [{
-        properties: {},
-        children: [
-            new Paragraph({
-                children: [
-                    new TextRun({ text: "Teste de negrito", bold: true }),
-                    new TextRun({ text: "\nTexto normal" })
-                ]
-            })
-        ]
-    }]
-});
+        sections: [{
+            properties: {},
+            children: [
+                new Paragraph({
+                    children: [
+                        new TextRun({ text: `Empresa: ${empresa}`),
+                        new TextRun(`\nNota Fiscal: ${nota}`),
+                        new TextRun(`\nOrdem de C/S: ${ordem}`),
+                        new TextRun(`\nFaturamento Mês/Ano: ${data}`)
+                    ]
+                }),
+                ...imagens.map(imgSrc => new Paragraph({
+                    children: [new ImageRun({
+                        data: imgSrc,
+                        transformation: { width: 400, height: 200 }
+                    })]
+                }))
+            ]
+        }]
+    });
 
 
     // Gerar e baixar o arquivo
